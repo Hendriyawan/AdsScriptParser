84e6ba8ac8ef6d462eacbd4ba3753b513c50671a
hendriyawan <hendrijs44@gmail.com>
2018-04-02 11:24:13 +0000

    Update UnitId.java

--- app/src/main/java/com/glanexdev/blog/adsscript/parser/UnitId.java
+++ app/src/main/java/com/glanexdev/blog/adsscript/parser/UnitId.java
@@ -2,7 +2,7 @@ package com.glanexdev.blog.adsscript.parser;
 
 public class UnitId
 {
-	public static String BANNER = "ca-app-pub-9788177404753370/4800414390";
-	public static String INTERSTITIAL = "ca-app-pub-9788177404753370/6606065057";
-	public static String APP_UNIT_ID = "ca-app-pub-9788177404753370~8735789978";
+	public static String BANNER = "ADS UNIT ID BANNER";
+	public static String INTERSTITIAL = "ADS UNIT ID INTERSTITIAL";
+	public static String APP_UNIT_ID = "ADS UNIT ID APP";
 }
